17418a32b0d94495b6272d7a364a606f9d6e0725
Tyreum <gabriel.kmn@gmail.com>
2019-10-01 22:20:51 -0300

    exercicio 2 inicio

---
+++ src/exerciciosjavaii/JavaEx02.java
@@ -0,0 +1,29 @@
+package exerciciosjavaii;
+import java.util.Scanner;
+
+public class JavaEx02 {
+    
+    public static void main(String[] args) {
+        
+        String nome = "", aux = "";
+        int nota = 0, média = 0, sala = 0, aviso = 0;
+        int tabelaMedias[][] = new int[6][3];
+        int tabelaResultado[][] = new int [3][1];
+        int tabelaAviso[][] = new int [3][1];
+        Scanner scan = new Scanner(System.in); 
+        for(int i = 0; i<4; i++){
+        
+            for(int o = 0; o<3; o++){
+                System.out.println(tabelaAviso[i][o]);
+            
+            }
+            
+        }
+        
+
+        
+  
+        
+    }
+    
+}
