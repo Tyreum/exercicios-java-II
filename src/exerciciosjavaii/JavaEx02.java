f3e3d051703f1f70f5fec2830ae0d89eaa27a3a0
Tyreum <gabriel.kmn@gmail.com>
2019-10-08 22:18:03 -0300

    sabendo o que não fazer

--- src/exerciciosjavaii/JavaEx02.java
+++ src/exerciciosjavaii/JavaEx02.java
@@ -5,12 +5,16 @@ public class JavaEx02 {
     
     public static void main(String[] args) {
         
-        String nome = "", aux = "";
-        int nota = 0, média = 0, sala = 0, aviso = 0;
+        String nome[], aux = "";
+        nome = new String [5];
+        int nota1[], nota2[] ,média = 0, sala = 0, aviso = 0;
+        nota1 = new int[5];
+        nota2 = new int[5];
         int tabelaMedias[][] = new int[6][3];
         int tabelaResultado[][] = new int [3][1];
         int tabelaAviso[][] = new int [3][1];
-        Scanner scan = new Scanner(System.in); 
+        Scanner scan = new Scanner(System.in);
+        
         for(int i = 0; i<4; i++){
         
             for(int o = 0; o<3; o++){
